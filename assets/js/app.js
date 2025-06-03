70006e82f1dce16512e0f9df9d8bcc9938cf78c5
ahad hossain <wpahad1997@gmail.com>
2025-06-03 19:39:29 +0600

    updatede

--- assets/js/app.js
+++ assets/js/app.js
@@ -186,30 +186,6 @@ function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e 
       }
     }]
   });
-  gsap.registerPlugin(ScrollTrigger);
-  var revealContainers = document.querySelectorAll(".zion-p-thumb");
-  revealContainers.forEach(function (container) {
-    var image = container.querySelector("img");
-    var tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: container,
-        toggleActions: "restart none none reset"
-      }
-    });
-    tl.set(container, {
-      autoAlpha: 1
-    });
-    tl.from(container, 1.5, {
-      xPercent: -70,
-      ease: Power2.out
-    });
-    tl.from(image, 1.5, {
-      xPercent: 70,
-      scale: 1.3,
-      delay: -1.5,
-      ease: Power2.out
-    });
-  });
 
   /*--------------------------------------------------------------
   ZION AOS ANIMATION JS INIT
@@ -218,45 +194,6 @@ function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e 
   AOS.init({
     once: true // Ensure animations can trigger multiple times
   });
-
-  /////////////////////////////////////////////////////
-  var text_animation = gsap.utils.toArray(".move-anim");
-  if (text_animation) {
-    text_animation.forEach(function (splitTextLine) {
-      var delay_value = 0.1;
-      if (splitTextLine.getAttribute("data-delay")) {
-        delay_value = splitTextLine.getAttribute("data-delay");
-      }
-      var tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: splitTextLine,
-          start: "top 85%",
-          duration: 1.3,
-          scrub: false,
-          markers: false,
-          toggleActions: "play none none none"
-        }
-      });
-      var itemSplitted = new SplitText(splitTextLine, {
-        type: "lines"
-      });
-      gsap.set(splitTextLine, {
-        perspective: 400
-      });
-      itemSplitted.split({
-        type: "lines"
-      });
-      tl.from(itemSplitted.lines, {
-        duration: 1,
-        delay: delay_value,
-        opacity: 0,
-        rotationX: -80,
-        force3D: true,
-        transformOrigin: "top center -50",
-        stagger: 0.1
-      });
-    });
-  }
 
   /*--------------------------------------------------------------
   ZION BRAND JS INIT
